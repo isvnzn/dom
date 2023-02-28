88a484def373aaa1c6ee5ae3202c089bbd77cc63
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-01 02:07:09 +0800

    Add paragraph inside container

---
+++ dom.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".container");
+
+const paragraph = document.createElement("p");
+paragraph.textContent = "Hey I'm red!";
+paragraph.style.color = "red";
+
+container.appendChild(paragraph);
