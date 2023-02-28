d06d156faee08086072985579824e1d76957871a
vinz <123943529+vcazu@users.noreply.github.com>
2023-03-01 02:07:41 +0800

    Add h3 inside container

--- dom.js
+++ dom.js
@@ -5,3 +5,9 @@ paragraph.textContent = "Hey I'm red!";
 paragraph.style.color = "red";
 
 container.appendChild(paragraph);
+
+const headingBlue = document.createElement("h3");
+headingBlue.textContent = "I'm a blue h3!";
+headingBlue.style.color = "blue";
+
+container.appendChild(headingBlue);
